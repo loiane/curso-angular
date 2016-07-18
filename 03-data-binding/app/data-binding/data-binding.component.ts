5539acb66549527d8b04e30d97597cf4c9eb0562
loiane <loianeg@gmail.com>
2016-07-18 11:09:47 -0300

    aula 11 - event binding

--- 03-data-binding/app/data-binding/data-binding.component.ts
+++ 03-data-binding/app/data-binding/data-binding.component.ts
@@ -4,6 +4,15 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'exemplo-data-binding',
   templateUrl: 'data-binding.component.html',
+  /*styles: [
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `
+  ]*/
+  styleUrls: ['data-binding.component.css']
 })
 export class DataBindingComponent {
   constructor() {  }
@@ -11,7 +20,29 @@ export class DataBindingComponent {
   url = 'http://loiane.com';
   urlImg = 'http://lorempixel.com/400/200/nature/';
 
+  conteudoAtual : string = '';
+  conteudoSalvo : string = '';
+
+  isMouseOver = false;
+
   getValor(){
     return 1;
+  }
+
+  onClick(){
+    alert('Botão clicado!');
+  }
+
+  onKeyup(event:KeyboardEvent){
+    console.log(event);
+    this.conteudoAtual = (<HTMlInputElement>event.target).value;
+  }
+
+  onSave(valor : string){
+    this.conteudoSalvo = valor;
+  }
+
+  onMouseSpan(){
+    this.isMouseOver = !this.isMouseOver;
   }
 }
