fb36a4c8f8cdb774bf5b655d01e435be1d456188
loiane <loianeg@gmail.com>
2016-08-09 10:35:04 -0300

    aula 17

---
+++ _04-diretivas/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
