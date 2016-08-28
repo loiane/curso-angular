7b3e9a543228c31e9ef39564da07061c08d3603b
loiane <loianeg@gmail.com>
2016-08-28 10:51:42 -0300

    removed teste component

--- _04-diretivas/src/app/app.module.ts
+++ _04-diretivas/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TesteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
