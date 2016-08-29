ab9f912ba28a3f1ed8e8d60a7fabbed7bff67e1c
loiane <loianeg@gmail.com>
2016-08-29 13:56:05 -0300

    aula 20 - ng lint e ng test

--- _04-diretivas/src/app/app.module.ts
+++ _04-diretivas/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule,
---
+++ _04-diretivas/src/app/teste/teste.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  templateUrl: 'teste.component.html',
+  styleUrls: ['teste.component.scss']
+})
+export class TesteComponent implements OnInit {
+
+  minhaVariavel: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
