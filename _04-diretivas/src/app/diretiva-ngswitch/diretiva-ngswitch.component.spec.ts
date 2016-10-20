7b6860ff999d5ad65c23e1b16c9c3a37a2a2db60
loiane <loianeg@gmail.com>
2016-10-20 15:52:05 -0200

    aula 26: ngSwitch

---
+++ _04-diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch.component';
+
+describe('Component: DiretivaNgswitch', () => {
+  it('should create an instance', () => {
+    let component = new DiretivaNgswitchComponent();
+    expect(component).toBeTruthy();
+  });
+});
