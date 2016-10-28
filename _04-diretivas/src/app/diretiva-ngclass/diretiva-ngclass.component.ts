6571410626e638b2122b7ca45478eb6308378e14
loiane <loianeg@gmail.com>
2016-10-28 12:02:20 -0200

    added aula 28 ngClass

--- _04-diretivas/src/app/diretiva-ngclass/diretiva-ngclass.component.ts
+++ _04-diretivas/src/app/diretiva-ngclass/diretiva-ngclass.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgclassComponent implements OnInit {
 
+  meuFavorito: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(){
+    this.meuFavorito = !this.meuFavorito;
   }
 
 }
