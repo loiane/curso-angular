37b6137c838c6f4b144736844951b9357a28a649
loiane <loianeg@gmail.com>
2016-11-08 11:12:08 -0200

    #35: Diretivas: Input e Property Binding

---
+++ _04-diretivas/src/app/shared/highlight.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { HighlightDirective } from './highlight.directive';
+
+describe('Directive: Highlight', () => {
+  it('should create an instance', () => {
+    let directive = new HighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
