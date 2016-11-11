5e30d18398142e660ac80c7d42f44ef9a07a47c5
loiane <loianeg@gmail.com>
2016-11-11 10:14:27 -0200

    #38: Criando um serviço (Service)

---
+++ _05-servicos/src/app/cursos/cursos.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  cursos: string[] = [];
+  //cursosService: CursosService;
+
+  constructor(private cursosService: CursosService) { 
+    //this.cursosService = new CursosService();
+    //this.cursosService = _cursosService;
+  }
+
+  ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
+  }
+
+}
