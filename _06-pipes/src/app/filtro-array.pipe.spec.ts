841a3a9fd3fd0b70efd36bd364e1e5b9444effce
loiane <loianeg@gmail.com>
2016-12-03 09:57:50 -0200

    #46: Pipes: Criando um Pipe "Puro"

---
+++ _06-pipes/src/app/filtro-array.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+
+describe('Pipe: FiltroArray', () => {
+  it('create an instance', () => {
+    let pipe = new FiltroArrayPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ _06-pipes/src/app/filtro-array.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filtroArray'
+})
+export class FiltroArrayPipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+
+    if (value.length === 0 || args === undefined){
+      return value;
+    }
+
+    let filter = args.toLocaleLowerCase();
+    return value.filter(
+      v => v.toLocaleLowerCase().indexOf(filter) != -1
+    );
+  }
+
+}
