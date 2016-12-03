5966ae8ac1131f703769575dd303bf70d0093224
loiane <loianeg@gmail.com>
2016-12-03 09:57:59 -0200

    #48: Pipes: Async (assíncrono)

--- _06-pipes/src/app/app.module.ts
+++ _06-pipes/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe,
+    FiltroArrayImpuroPipe
   ],
   imports: [
     BrowserModule,
--- _06-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ _06-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -15,6 +16,37 @@ export class ExemplosPipesComponent implements OnInit {
     dataLancamento: new Date(2016, 5, 23),
     url: 'http://a.co/glqjpRP'
   };
+
+  livros: string[] = ['Java', 'Angular 2'];
+
+  filtro: string;
+
+  addCurso(valor){
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
+
+  obterCursos(){
+
+    if (this.livros.length === 0 || this.filtro === undefined
+    || this.filtro.trim() === ''){
+      return this.livros;
+    }
+
+     return this.livros.filter((v) => {
+      if (v.toLowerCase().indexOf(this.filtro.toLowerCase()) >= 0) {
+        return true;
+      }
+      return false;
+    });
+  }
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  });
+
+  valorAsync2 = Observable.interval(2000)
+    .map(valor => 'Valor assíncrono 2');
 
   constructor() { }
 
