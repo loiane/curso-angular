3820fe4f791c4bce96d90ec943a91018caeb84d8
loiane <loianeg@gmail.com>
2017-03-16 10:17:31 -0300

    removendo projeto de aula antiga - desatualizado

--- 02-primeiro-componente/app/cursos/cursos.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class CursosService {
-
-  getCursos(){
-    return ['Angular 2', 'Java',  'Ext JS', 'Estrutura de Dados'];
-  }
-}
--- 02-primeiro-componente/app/primeiro/meu-primeiro.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'meu-primeiro-componente',
-  template: '<h2>Meu primeiro componente Angular 2</h2>'
-})
-export class MeuPrimeiroComponent {}
