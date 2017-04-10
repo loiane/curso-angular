9ec08d2952b402c3e8972b42a20a390adcf31a3e
loiane <loianeg@gmail.com>
2017-04-10 12:34:06 -0300

    aula 74: Forms (template driven) ngForm, ngSubmit e ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  onSubmit(form){
+    console.log(form);
+  }
+
   constructor() { }
 
   ngOnInit() {
