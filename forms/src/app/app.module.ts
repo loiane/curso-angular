bb41e16af26fa9560487f1a6525fa34a92abe892
loiane <loianeg@gmail.com>
2017-04-10 12:48:47 -0300

    aula 76: Forms (template driven) Módulos e FormsModule

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TemplateFormModule } from './template-form/template-form.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,20 +6,19 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ forms/src/app/template-form/template-form.module.ts
@@ -0,0 +1,15 @@
+import { FormsModule } from '@angular/forms';
+import { TemplateFormComponent } from './template-form.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    TemplateFormComponent
+  ]
+})
+export class TemplateFormModule { }
