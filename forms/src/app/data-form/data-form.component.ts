30d06a5a4036942b47f9b62a37f5e7ecdbbefbdd
loiane <loianeg@gmail.com>
2018-07-04 10:26:35 -0400

    Combobox com Objeto (ngValue e compareWith)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -15,6 +15,7 @@ export class DataFormComponent implements OnInit {
   formulario: FormGroup;
   // estados: EstadoBr[];
   estados: Observable<EstadoBr[]>;
+  cargos: any[];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -26,6 +27,8 @@ export class DataFormComponent implements OnInit {
   ngOnInit() {
 
     this.estados = this.dropdownService.getEstadosBr();
+
+    this.cargos = this.dropdownService.getCargos();
 
     /*this.dropdownService.getEstadosBr().subscribe(dados => {
       this.estados = dados;
@@ -53,7 +56,9 @@ export class DataFormComponent implements OnInit {
         bairro: [null, Validators.required],
         cidade: [null, Validators.required],
         estado: [null, Validators.required]
-      })
+      }),
+
+      cargo: [null]
     });
 
     // tslint:disable-next-line:max-line-length
@@ -156,5 +161,14 @@ export class DataFormComponent implements OnInit {
         estado: null
       }
     });
+  }
+
+  setarCargo() {
+    const cargo = { nome: 'Dev', nivel: 'Pleno', desc: 'Dev Pl' };
+    this.formulario.get('cargo').setValue(cargo);
+  }
+
+  compararCargos(obj1, obj2) {
+    return obj1 && obj2 ? (obj1.nome === obj2.nome && obj1.nivel === obj2.nivel) : obj1 === obj2;
   }
 }
--- forms/src/app/shared/services/dropdown.service.ts
+++ forms/src/app/shared/services/dropdown.service.ts
@@ -9,4 +9,12 @@ export class DropdownService {
   getEstadosBr() {
     return this.http.get<EstadoBr[]>('assets/dados/estadosbr.json');
   }
+
+  getCargos() {
+    return [
+      { nome: 'Dev', nivel: 'Junior', desc: 'Dev Jr' },
+      { nome: 'Dev', nivel: 'Pleno', desc: 'Dev Pl' },
+      { nome: 'Dev', nivel: 'Senior', desc: 'Dev Sr' }
+    ];
+  }
 }
