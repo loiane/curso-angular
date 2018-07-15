6a17413f586ffee1bee748c0046354a1d4616be4
loiane <loianeg@gmail.com>
2018-07-15 15:53:23 -0400

    107: validacao customizada: checkboxes

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 import { EstadoBr } from './../shared/models/estado-br.model';
 import { DropdownService } from './../shared/services/dropdown.service';
 import { ConsultaCepService } from '../shared/services/consulta-cep.service';
 import { Observable } from 'rxjs';
+import { FormValidations } from '../shared/form-validations';
 
 @Component({
   selector: 'app-data-form',
@@ -78,7 +79,7 @@ export class DataFormComponent implements OnInit {
 
   buildFrameworks() {
     const values = this.frameworks.map(v => new FormControl(false));
-    return this.formBuilder.array(values);
+    return this.formBuilder.array(values, FormValidations.requiredMinCheckbox(1));
     /* this.formBuilder.array( [
       new FormControl(false), // angular
       new FormControl(false), // react
---
+++ forms/src/app/shared/form-validations.ts
@@ -0,0 +1,21 @@
+import { FormArray } from '@angular/forms';
+
+export class FormValidations {
+
+  static requiredMinCheckbox(min = 1) {
+    const validator = (formArray: FormArray) => {
+      /* const values = formArray.controls;
+      let totalChecked = 0;
+      for (let i = 0; i < values.length; i++) {
+        if (values[i].value) {
+          totalChecked += 1;
+        }
+      } */
+      const totalChecked = formArray.controls
+        .map(v => v.value)
+        .reduce((total, current) => current ? total + current : total, 0);
+      return totalChecked >= min ? null : { required: true };
+    };
+    return validator;
+  }
+}
