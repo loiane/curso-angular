bcf8b7264e5ad7fb8ca39df1ee36fc6e3daf8428
loiane <loianeg@gmail.com>
2018-07-15 16:08:20 -0400

    108: validacao cep

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -56,7 +56,7 @@ export class DataFormComponent implements OnInit {
       email: [null, [Validators.required, Validators.email]],
 
       endereco: this.formBuilder.group({
-        cep: [null, Validators.required],
+        cep: [null, [Validators.required, FormValidations.cepValidator]],
         numero: [null, Validators.required],
         complemento: [null],
         rua: [null, Validators.required],
@@ -137,6 +137,13 @@ export class DataFormComponent implements OnInit {
   verificaValidTouched(campo: string) {
     return (
       !this.formulario.get(campo).valid &&
+      (this.formulario.get(campo).touched || this.formulario.get(campo).dirty)
+    );
+  }
+
+  verificaRequired(campo: string) {
+    return (
+      this.formulario.get(campo).hasError('required') &&
       (this.formulario.get(campo).touched || this.formulario.get(campo).dirty)
     );
   }
--- forms/src/app/shared/form-validations.ts
+++ forms/src/app/shared/form-validations.ts
@@ -1,4 +1,4 @@
-import { FormArray } from '@angular/forms';
+import { FormArray, FormControl } from '@angular/forms';
 
 export class FormValidations {
 
@@ -17,5 +17,15 @@ export class FormValidations {
       return totalChecked >= min ? null : { required: true };
     };
     return validator;
+  }
+
+  static cepValidator(control: FormControl) {
+
+    const cep = control.value;
+    if (cep && cep !== '') {
+      const validacep = /^[0-9]{8}$/;
+      return validacep.test(cep) ? null : { cepInvalido : true };
+    }
+    return null;
   }
 }
