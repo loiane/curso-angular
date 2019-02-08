9aeb5523eac7a3cd7854e8f92f1f255d2410ae16
Loiane <loianeg@gmail.com>
2019-02-08 18:45:30 -0500

    aula 124

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
-import { Observable } from 'rxjs';
+import { Observable, empty, of, Subject } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -14,6 +15,7 @@ export class CursosListaComponent implements OnInit {
   // cursos: Curso[];
 
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
@@ -21,7 +23,33 @@ export class CursosListaComponent implements OnInit {
     // this.service.list()
     // .subscribe(dados => this.cursos = dados);
 
-    this.cursos$ = this.service.list();
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.service.list()
+    .pipe(
+      // map(),
+      // tap(),
+      // switchMap(),
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
+
+    this.service.list()
+    .pipe(
+      catchError(error => empty())
+    )
+    .subscribe(
+      dados => {
+        console.log(dados);
+      }
+      // ,error => console.error(error),
+      // () => console.log('Obserservable completo!')
+    );
   }
 
 }
