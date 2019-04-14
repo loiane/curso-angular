3fae797373ea04089c434edcba1b028f6277173e
Loiane <loianeg@gmail.com>
2019-04-14 16:27:52 -0400

    file upload node backend

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multipart = require('connect-multiparty');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const multipartMiddleware = multipart({ uploadDir: './uploads' });
+app.post('/upload', multipartMiddleware, (req, res) => {
+  const files = req.files;
+  console.log(files);
+  res.json({ message: files });
+});
+
+app.use((err, req, res, next) => res.json({error: err.message}));
+
+app.listen(8000, () => {
+  console.log('Servidor porta 8000');
+})
