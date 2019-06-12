cb9ace5feda58eca2c657c7a463f98e4c9b214ef
Loiane <loianeg@gmail.com>
2019-06-12 16:47:28 -0400

    angular v8

--- forms/src/polyfills.ts
+++ forms/src/polyfills.ts
@@ -38,7 +38,7 @@
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
+// import 'core-js/es6/reflect';
 
 
 /**
