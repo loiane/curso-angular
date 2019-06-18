e8bc30a21af08dde3a2f28eb6bc4bedb9a47a496
Loiane <loianeg@gmail.com>
2019-06-18 17:58:27 -0400

    aula 137

---
+++ requests-http/src/app/upload-file/upload-file.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadFileService {
+
+  constructor(private http: HttpClient) { }
+
+  upload(files: Set<File>, url: string) {
+
+    const formData = new FormData();
+    files.forEach(file => formData.append('file', file, file.name));
+
+    // const request = new HttpRequest('POST', url, formData);
+    // return this.http.request(request);
+
+    return this.http.post(url, formData);
+  }
+}
--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-upload-file',
@@ -7,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  files: Set<File>;
 
-  ngOnInit() {
-  }
+  constructor(private service: UploadFileService) { }
+
+  ngOnInit() { }
 
   onChange(event) {
     console.log(event);
@@ -19,9 +22,18 @@ export class UploadFileComponent implements OnInit {
     // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
 
     const fileNames = [];
+    this.files = new Set();
     for (let i = 0; i < selectedFiles.length; i++) {
       fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
     }
     document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+  }
+
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.service.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe(response => console.log('Upload Concluído'));
+    }
   }
 }
