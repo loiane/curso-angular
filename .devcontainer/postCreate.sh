3924efc6f294b3e074e1bcfbb3a22ad677c25960
Loiane Groner <loianeg@gmail.com>
2020-07-26 16:17:45 +0000

    add arquivos de config para Visual Studio Codestapces

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~/workspace/angularv61
+npm i
+cd ~/workspace/data-binding
+npm i
+cd ~/workspace/data-binding
+npm i
+cd ~/workspace/forms
+npm i
+cd ~/workspace/novidades-v9
+npm i
+cd ~/workspace/pipes
+npm i
+cd ~/workspace/primeiro-projeto
+npm i
+cd ~/workspace/requests-http
+npm i
+cd ~/workspace/servicos
+npm i
+cd ~/workspace
