e12d16bea70176d6a6edef429e8435ef1cbab440
Loiane <loianeg@gmail.com>
2021-08-22 19:23:26 -0400

    crud aula 02

--- crud-angular-spring/crud-angular/src/app/app.module.ts
+++ crud-angular-spring/crud-angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
