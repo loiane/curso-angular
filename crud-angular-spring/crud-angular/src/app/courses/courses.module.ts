bae750829958442fa1b95c62b1b97de1acb67588
Loiane <loianeg@gmail.com>
2021-08-26 12:17:00 -0400

    curd angular + Spring - aula 07

--- crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
@@ -13,7 +13,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
---
+++ crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [
+    MatCardModule,
+    MatTableModule,
+    MatToolbarModule
+  ],
+})
+export class AppMaterialModule { }
