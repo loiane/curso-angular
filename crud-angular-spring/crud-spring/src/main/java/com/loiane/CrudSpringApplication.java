bbae7896e1437218c99f8be84a069d0b845768ab
Loiane <loianeg@gmail.com>
2021-11-13 12:16:37 -0500

    aula 14 - add projeto spring

---
+++ crud-angular-spring/crud-spring/src/main/java/com/loiane/CrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.loiane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
---
+++ crud-angular-spring/crud-spring/src/test/java/com/loiane/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loiane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
