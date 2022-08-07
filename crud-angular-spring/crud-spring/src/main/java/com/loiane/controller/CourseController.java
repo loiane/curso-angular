fdb2e0e972b9c40912155ea33ea5653050a1d84a
Loiane <loianeg@gmail.com>
2022-08-07 15:45:05 -0400

    26: API get by id

--- crud-angular-spring/crud-spring/src/main/java/com/loiane/controller/CourseController.java
+++ crud-angular-spring/crud-spring/src/main/java/com/loiane/controller/CourseController.java
@@ -8,6 +8,7 @@ import com.loiane.repository.CourseRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,20 +26,27 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    //@RequestMapping(method = RequestMethod.GET)
+    // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }
 
-    //@RequestMapping(method = RequestMethod.POST)
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         // System.out.println(course.getName());
         return courseRepository.save(course);
         // return ResponseEntity.status(HttpStatus.CREATED)
-        //     .body(courseRepository.save(course));
+        // .body(courseRepository.save(course));
     }
 
 }
