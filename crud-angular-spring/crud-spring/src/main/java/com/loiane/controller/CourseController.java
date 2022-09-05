e1570e9fcb1243e29c1682d287d741fd306d9081
Loiane <loianeg@gmail.com>
2022-09-05 18:04:57 -0400

    31: http delete spring

--- crud-angular-spring/crud-spring/src/main/java/com/loiane/controller/CourseController.java
+++ crud-angular-spring/crud-spring/src/main/java/com/loiane/controller/CourseController.java
@@ -7,6 +7,7 @@ import com.loiane.repository.CourseRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,16 @@ public class CourseController {
                     recordFound.setCategory(course.getCategory());
                     Course updated = courseRepository.save(recordFound);
                     return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
