0e1aa5f4a304c2394ef5a652a300bcf42f3a65e3
Loiane <loianeg@gmail.com>
2022-12-15 12:24:42 -0500

    aula 36: remoção lógica

--- crud-angular-spring/crud-spring/src/main/java/com/loiane/model/Course.java
+++ crud-angular-spring/crud-spring/src/main/java/com/loiane/model/Course.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -19,6 +21,8 @@ import lombok.Data;
 @Data
 @Entity
 // @Table(name = "cursos")
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
     @Id
@@ -38,4 +42,9 @@ public class Course {
     @Column(length = 10, nullable = false)
     private String category;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 }
