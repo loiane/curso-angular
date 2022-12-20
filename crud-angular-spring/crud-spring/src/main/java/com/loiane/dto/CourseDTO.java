b510b4eeeac622746dbd5bd1c24e3e82e71cc6f4
Loiane <loianeg@gmail.com>
2022-12-20 16:55:07 -0500

    41 DTO

---
+++ crud-angular-spring/crud-spring/src/main/java/com/loiane/dto/CourseDTO.java
@@ -0,0 +1,15 @@
+package com.loiane.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CourseDTO(
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 5, max = 100) String name,
+        @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category) {
+}
