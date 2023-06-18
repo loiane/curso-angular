8f1f309675ba7d9e022abb31990e9dfb15bb32a0
Loiane <loianeg@gmail.com>
2023-06-18 12:12:36 -0400

    51: add e remove aulas

--- crud-angular-spring/crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -60,6 +60,16 @@ export class CourseFormComponent implements OnInit {
     return (<UntypedFormArray>this.form.get('lessons')).controls;
   }
 
+  addNewLesson() {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    lessons.push(this.createLesson());
+  }
+
+  removeLesson(index: number) {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    lessons.removeAt(index);
+  }
+
   onSubmit() {
     this.service.save(this.form.value)
       .subscribe(result => this.onSuccess(), error => this.onError());
