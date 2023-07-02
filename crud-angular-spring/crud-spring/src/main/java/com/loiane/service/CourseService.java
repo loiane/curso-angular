5a293cc7537272ae1fd6e4a9f3071ca083ceba63
Loiane <loianeg@gmail.com>
2023-07-02 14:56:33 -0400

    55: create e update aulas

--- crud-angular-spring/crud-spring/src/main/java/com/loiane/service/CourseService.java
+++ crud-angular-spring/crud-spring/src/main/java/com/loiane/service/CourseService.java
@@ -50,7 +50,6 @@ public class CourseService {
                     Course course = courseMapper.toEntity(courseDTO);
                     recordFound.setName(courseDTO.name());
                     recordFound.setCategory(courseMapper.convertCategoryValue(courseDTO.category()));
-                    // recordFound.setLessons(course.getLessons());
                     recordFound.getLessons().clear();
                     course.getLessons().forEach(recordFound.getLessons()::add);
                     return courseMapper.toDTO(courseRepository.save(recordFound));
